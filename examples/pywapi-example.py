933b53c781cdaf5a64ae0c215343d75b3cb1f96e
mydimle <mydimle@f8314ed2-3ed7-11de-b232-599c1b3c59e8>
2012-05-09 12:40:25 +0000

    Examples: Updated to work with Python 3.

--- examples/pywapi-example.py
+++ examples/pywapi-example.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
-import  pywapi
-import string
+import pywapi
 
 google_result = pywapi.get_weather_from_google('10001')
 yahoo_result = pywapi.get_weather_from_yahoo('10001')
 noaa_result = pywapi.get_weather_from_noaa('KJFK')
 
-print "Google says: It is " + string.lower(google_result['current_conditions']['condition']) + " and " + google_result['current_conditions']['temp_c'] + "C now in New York.\n\n"
+print("Google says: It is " + google_result['current_conditions']['condition'].lower() + " and " + google_result['current_conditions']['temp_c'] + "C now in New York.")
 
-print "Yahoo says: It is " + string.lower(yahoo_result['condition']['text']) + " and " + yahoo_result['condition']['temp'] + "C now in New York.\n\n"
+print("Yahoo says: It is " + yahoo_result['condition']['text'].lower() + " and " + yahoo_result['condition']['temp'] + "C now in New York.")
 
-print "NOAA says: It is " + string.lower(noaa_result['weather']) + " and " + noaa_result['temp_c'] + "C now in New York.\n"
+print("NOAA says: It is " + noaa_result['weather'].lower() + " and " + noaa_result['temp_c'] + "C now in New York.")
