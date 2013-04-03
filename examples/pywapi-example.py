ab2a8529bffd04523b5fb75a140691cd28e4b0f6
jtasker@gmail.com <jtasker@gmail.com@f8314ed2-3ed7-11de-b232-599c1b3c59e8>
2013-04-03 13:40:27 +0000

    Updated license, readme, and main example script

--- examples/pywapi-example.py
+++ examples/pywapi-example.py
@@ -2,8 +2,11 @@
 
 import pywapi
 
+weather_com_result = pywapi.get_weather_from_weather_com('10001')
 yahoo_result = pywapi.get_weather_from_yahoo('10001')
 noaa_result = pywapi.get_weather_from_noaa('KJFK')
+
+print "Weather.com says: It is " + string.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in New York.\n\n"
 
 print("Yahoo says: It is " + yahoo_result['condition']['text'].lower() + " and " + yahoo_result['condition']['temp'] + "C now in New York.")
 
