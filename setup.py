d6998ffa4b7fee4cbb7700c59129fb8ac9b9bec4
jtasker@gmail.com <jtasker@gmail.com@f8314ed2-3ed7-11de-b232-599c1b3c59e8>
2013-04-03 13:47:11 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 
 from distutils.core import setup
 
-__author__ = 'qetzal@gmail.com'
+__author__ = 'qetzal@gmail.com, jtasker@gmail.com'
 __version__ = '0.3'
 
 
 setup(name='pywapi',
     version=__version__,
     description='A python wrapper around the Yahoo! Weather, Google Weather and NOAA APIs',
-    author='Eugene Kaznacheev',
-    author_email='qetzal@gmail.com',
+    author='Eugene Kaznacheev, Joshua Tasker',
+    author_email='qetzal@gmail.com, jtasker@gmail.com',
     url='http://code.google.com/p/python-weather-api/',
     py_modules=['pywapi'],
     license='MIT',
