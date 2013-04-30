c9bc78969a8b0b9f2bcc2d885e6fbcfd70a42933
jtasker@gmail.com <jtasker@gmail.com@f8314ed2-3ed7-11de-b232-599c1b3c59e8>
2013-04-30 01:23:56 +0000

    Add long description

--- setup.py
+++ setup.py
@@ -32,11 +32,17 @@ __version__ = '0.3'
 
 setup(name='pywapi',
     version=__version__,
-    description='A python wrapper around the Yahoo! Weather, Weather.com and NOAA APIs',
+    description='Python wrapper around different weather APIs',
     author='Eugene Kaznacheev, Joshua Tasker',
     author_email='qetzal@gmail.com, jtasker@gmail.com',
     url='http://code.google.com/p/python-weather-api/',
     py_modules=['pywapi'],
     license='MIT',
     keywords = 'weather api yahoo noaa google',
+    platforms = 'any',
+    long_description = """
+This module provides a Python wrapper around the Yahoo! Weather, Weather.com,
+and National Oceanic and Atmospheric Administration (NOAA) APIs. Fetch
+weather reports using zip code, location id, city name, state, country, etc.
+    """
 )
