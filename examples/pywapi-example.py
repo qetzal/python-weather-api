5805f84500ebca762ec2c7e34f344bf3c2406a8d
jtasker@gmail.com <jtasker@gmail.com@f8314ed2-3ed7-11de-b232-599c1b3c59e8>
2013-05-22 01:54:30 +0000

    Fix error in example script

--- examples/pywapi-example.py
+++ examples/pywapi-example.py
@@ -6,7 +6,7 @@ weather_com_result = pywapi.get_weather_from_weather_com('10001')
 yahoo_result = pywapi.get_weather_from_yahoo('10001')
 noaa_result = pywapi.get_weather_from_noaa('KJFK')
 
-print "Weather.com says: It is " + string.lower(weather_com_result['current_conditions']['text']) + " and " + weather_com_result['current_conditions']['temperature'] + "C now in New York.\n\n"
+print "Weather.com says: It is " + weather_com_result['current_conditions']['text'].lower() + " and " + weather_com_result['current_conditions']['temperature'] + "C now in New York."
 
 print("Yahoo says: It is " + yahoo_result['condition']['text'].lower() + " and " + yahoo_result['condition']['temp'] + "C now in New York.")
 
